1b38b6693b9f709556ec4739be28c587f2ac27f5
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2017-12-18 14:38:12 +0000

    try simple node express

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const app = express()
 
-port = process.env.port || 5000
+port = process.env.port || 3000
 
-app.use(express.static(__dirname + '/client/build/'))
+//app.use(express.static(__dirname + '/client/build/'))
 
 //app.get('/', (req, res) => res.sendFile(__dirname + '/client/build/index.html'))
-app.get('/', (req,res) => res.send('JOSE'))
+app.get('/', (req, res) => res.send('JOSE'))
 //app.get('/api', (req, res) => res.send({ name: 'jose' }))
 
 app.listen(port)
