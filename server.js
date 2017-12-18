302da20652e9bf86c7ab636d13d423fb02ff61c7
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2017-12-18 14:40:21 +0000

    try new endpoint

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ port = process.env.port || 3000
 //app.use(express.static(__dirname + '/client/build/'))
 
 //app.get('/', (req, res) => res.sendFile(__dirname + '/client/build/index.html'))
-app.get('/', (req, res) => res.send('JOSE'))
+app.get('*', (req, res) => res.send('JOSE'))
 //app.get('/api', (req, res) => res.send({ name: 'jose' }))
 
 app.listen(port)
