980a31c32d16b070bc4f0eaee258542916ae41cf
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-01-16 16:47:02 +0000

    add new routes for addword and getvocabulary

--- server-config/api/routes/routes.js
+++ server-config/api/routes/routes.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const listControllers = require('../controllers/controllers')
+const addWordController = require('../controllers/addnewword')
+const getVocabularyController = require('../controllers/getvocabulary')
 
 const apirouter = express.Router()
 
-apirouter.get('/getvocabulary/:file', listControllers.getvocabulary)
+apirouter.get('/getvocabulary', getVocabularyController.getvocabulary)
 
 apirouter.get('/getListOfTests', listControllers.getListOfTests)
 
 apirouter.post('/savetest', listControllers.savetest)
 
-apirouter.post('/addnewword', listControllers.addnewword)
+apirouter.post('/addnewword', addWordController.addnewword)
 
 apirouter.post('/removeword', listControllers.removeword)
 
